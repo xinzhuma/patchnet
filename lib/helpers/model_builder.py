c8ea43456bb22941f3f7f3e2a59cd56de12dd16c
XINZHU.MA <fgjhgju@qq.com>
2020-08-24 14:54:04 +0800

    Remove irrelevant code and modify the default settings

--- lib/helpers/model_builder.py
+++ lib/helpers/model_builder.py
@@ -1,11 +1,9 @@
 from lib.models.fpointnet import FPointNet
-from lib.models.am3d import AM3D
 from lib.models.patchnet import PatchNet
-from lib.models.patchnet_vanilla import PatchNet_Vanilla
 
 
 def build_model(cfg_model, dataset_helper, logger):
-    assert cfg_model['name'] in ['fpointnet', 'am3d', 'patchnet', 'patchnet_vanilla']
+    assert cfg_model['name'] in ['fpointnet', 'patchnet']
     if cfg_model['name'] =='fpointnet':
         return FPointNet(cfg=cfg_model,
                          num_heading_bin=dataset_helper.num_heading_bin,
@@ -16,10 +14,5 @@ def build_model(cfg_model, dataset_helper, logger):
                         num_heading_bin=dataset_helper.num_heading_bin,
                         num_size_cluster=dataset_helper.num_size_cluster,
                         mean_size_arr=dataset_helper.mean_size_arr)
-    elif cfg_model['name'] == 'am3d':
-        return AM3D(cfg=cfg_model,
-                    num_heading_bin=dataset_helper.num_heading_bin,
-                    num_size_cluster=dataset_helper.num_size_cluster,
-                    mean_size_arr=dataset_helper.mean_size_arr)
     else:
         raise NotImplementedError("%s model is not supported" % cfg_model['name'])
